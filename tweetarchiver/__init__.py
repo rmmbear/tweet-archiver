05a75ca598aab4dda80fd99a45f5249174630697
rmmbear <rmmbear@users.noreply.github.com>
2019-12-03 03:47:02 +0100

    database implementation - first pass
    also implements:
    - resolve isssue with logging format not sticking
    - scrape_tweets returns a generator yielding page of tweet objects
    - detect videos correctly
    - add type hinting

--- tweetarchiver/__init__.py
+++ tweetarchiver/__init__.py
@@ -1,9 +1,15 @@
 import time
 import logging
 from time import strftime
-
+from typing import Generator
 import requests
-import bs4
+
+import sqlalchemy as sqla
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+DeclarativeBase = declarative_base()
 
 __VERSION__ = "0.1"
 HTML_PARSER = "html.parser"
@@ -15,22 +21,137 @@ USER_AGENT = "".join(
 
 LOG_FORMAT_TERM = logging.Formatter("[%(levelname)s] %(message)s")
 LOG_FORMAT_FILE = logging.Formatter("[%(levelname)s] T+%(relativeCreated)d: %(name)s.%(funcName)s() line:%(lineno)d %(message)s")
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
 FH = logging.FileHandler("lastrun.log", mode="w")
 FH.setLevel(logging.DEBUG)
 FH.setFormatter(LOG_FORMAT_FILE)
 CH = logging.StreamHandler()
-CH.setLevel(logging.INFO)
+CH.setLevel(logging.DEBUG)
 CH.setFormatter(LOG_FORMAT_TERM)
-#LOGGER.addHandler(CH)
-LOGGER.addHandler(FH)
+logging.basicConfig(style="%", handlers=(FH, CH))
+LOGGER = logging.getLogger("tweetarchiver")
 
-def download(link, max_retries=3):
+
+class Tweet(DeclarativeBase):
+    """
+    """
+    __tablename__ = "account_archive"
+    tweet_id = sqla.Column(sqla.String, primary_key=True, nullable=False)
+    thread_id = sqla.Column(sqla.String, nullable=False)
+    timestamp = sqla.Column(sqla.Integer, nullable=False)
+
+    image_1_url = sqla.Column(sqla.String, nullable=True)
+    image_2_url = sqla.Column(sqla.String, nullable=True)
+    image_3_url = sqla.Column(sqla.String, nullable=True)
+    image_4_url = sqla.Column(sqla.String, nullable=True)
+
+    has_video = sqla.Column(sqla.Boolean, nullable=True)
+
+    text = sqla.Column(sqla.String, nullable=True)
+
+
+    def __init__(self, tweet_html: BeautifulSoup) -> None:
+        self.tweet_id = tweet_html.get("data-tweet-id").strip()
+        self.thread_id = tweet_html.get("data-conversation-id").strip()
+        self.timestamp = tweet_html.select(".js-short-timestamp")[0].get("data-time").strip()
+
+        #self.display_name = tweet_html.get("data-name")
+        #self.account_name = tweet_html.get("data-screen-name").strip()
+        #self.account_id = tweet_html.get("data-user-id").strip()
+
+        self._parse_media(tweet_html)
+
+        text_container = tweet_html.select(".js-tweet-text")[0]
+        text_container_str = str(text_container)
+
+        for element in text_container.select("p > *"):
+            if element.name == "a":
+                element_text = self._parse_link(element)
+            elif element.name == "span":
+                if "data-original-codepoint" in element.attrs:
+                    # as far as I know, this is only done for U+fe0f
+                    element_text = chr(int(element.get('data-original-codepoint')[2:], 16))
+                elif "twitter-hashflag-container" in element.attrs["class"]:
+                    # this is for promotional hashtags with special "emojis" (they're not actually emojis)
+                    a = element.find("a")
+                    element_text = a.text if a else ""
+                else:
+                    print(f"ID={self.tweet_id} SPAN NOT MATCHED")
+                    LOGGER.error("SPAN WAS NOT MATCHED IN ID %s", self.tweet_id)
+                    LOGGER.error("%s", element)
+            elif element.name == "img":
+                # this is for emojis - grab the alt text containing actual unicode text
+                # and disregard the image
+                element_text = element.get("alt")
+            else:
+                print(f"ID={self.tweet_id} TAG UNEXPECTED")
+                LOGGER.error("TAG WAS UNEXPECTED IN ID %s", self.tweet_id)
+                LOGGER.error("%s", element)
+
+            text_container_str = text_container_str.replace(str(element), element_text, 1)
+
+        text_container = BeautifulSoup(text_container_str, HTML_PARSER)
+        self.text = text_container.text
+        if not self.text:
+            self.text = None
+
+    def _parse_media(self, tweet_html: BeautifulSoup) -> None:
+        images = []
+        videos = []
+
+        media_container = tweet_html.select(".js-stream-tweet .AdaptiveMediaOuterContainer", limit=1)
+        if not media_container:
+            return
+        media_container = media_container[0]
+
+        # check if there can be other tags in addition to video and image
+        for num, image_element in enumerate(media_container.select("img")):
+            image_url = image_element.get("src").strip()
+            setattr(self, f"image_{num+1}_url", image_url)
+            images.append(image_url)
+        for num, video_element in enumerate(media_container.select(".is-video")):
+            self.has_video = True
+            videos.append("video")
+            assert num == 0
+            assert video_element
+
+        assert len(images) <= 4
+        assert len(videos) <= 1
+        assert True if not videos else len(images) == 0
+
+
+    def _parse_link(self, element: BeautifulSoup) -> str:
+        """
+        """
+        if "twitter-atreply" in element.attrs["class"]:
+            element_text = element.text
+        elif "twitter-hashtag" in element.attrs["class"]:
+            element_text = element.text
+        elif "twitter-cashtag" in element.attrs["class"]:
+            element_text = element.text
+        elif "twitter-timeline-link" in element.attrs["class"]:
+            if "data-expanded-url" in element.attrs:
+                element_text = element.get("data-expanded-url")
+                if "u-hidden" in element.attrs["class"]:
+                    # add padding to avoid smashing into text
+                    element_text = f" {element_text}"
+            elif "data-pre-embedded" in element.attrs and element.attrs["data-pre-embedded"] == "true":
+                element_text = ""
+
+            else:
+                LOGGER.error("TIMELINE LINK WAS NOT MATCHED IN ID %s", self.tweet_id)
+
+        else:
+            print(f"ID={self.tweet_id} LINK NOT MATCHED")
+            LOGGER.error("LINK WAS NOT MATCHED IN ID %s", self.tweet_id)
+            LOGGER.error("%s", element)
+
+        return element_text
+
+
+def download(link: str, max_retries: int = 3) -> str:
     """
     """
     retry_count = 0
-    #mmm... spagetti
     while True:
         try:
             response = requests.get(
@@ -40,7 +161,7 @@ def download(link, max_retries=3):
             if retry_count >= max_retries:
                 break
 
-            print(" Retrying ({}/{})".format(retry_count, max_retries))
+            print(f" Retrying({retry_count}/{max_retries})")
             continue
 
         if response.status_code != 200:
@@ -50,7 +171,7 @@ def download(link, max_retries=3):
                 break
 
             retry_count += 1
-            print(" Retrying({}/{})".format(retry_count, max_retries))
+            print(f" Retrying({retry_count}/{max_retries})")
             continue
 
         return response.text
@@ -59,7 +180,8 @@ def download(link, max_retries=3):
     return ""
 
 
-def scrape_tweet_ids(id_file, username, since_id=0, max_id=0, page_limit=0, page_delay=1.5):
+def scrape_tweets(username: str, since_id: int = 0, max_id: int = 0,
+                  page_limit: int = 0, page_delay: float = 1.5) -> Generator[Tweet, None, None]:
     """Scrape the  twitter feed using twitter's search to work around
     the API 3.2k status lookup limit.
 
@@ -74,6 +196,7 @@ def scrape_tweet_ids(id_file, username, since_id=0, max_id=0, page_limit=0, page
     query_template = query_template.format(username)
     page_number = 1
     time_last = 0
+    tweets_found = 0
     while True:
         query_url = query_template
         if since_id:
@@ -82,98 +205,30 @@ def scrape_tweet_ids(id_file, username, since_id=0, max_id=0, page_limit=0, page
             query_url = "".join([query_url, " max_id:", str(max_id)])
 
         print("Scraping page", page_number, ":", query_url)
-        # rate limit to 1 request per page_delay seconds (1.5s by default)
+        # rate limit to 1 request per page_delay seconds
         # use max() as a clamp allowing only 0 and positive values
         time.sleep(max(0, time_last + page_delay - time.time()))
         time.last = time.time()
         results_page = download(query_url)
-        parsed_page = bs4.BeautifulSoup(results_page, HTML_PARSER)
+        results_page = BeautifulSoup(results_page, HTML_PARSER)
         max_id = 0
-        for tweet in parsed_page.select(".js-stream-tweet"):
-            tweet_id, tweet_line = parse_tweet(tweet)
-            id_file.write(tweet_line)
-            id_file.write(";\r\n")
-            max_id = tweet_id
+        new_tweets = []
+        for tweet_html in results_page.select(".js-stream-tweet"):
+            tweet = Tweet(tweet_html)
+            max_id = tweet.tweet_id
+            new_tweets.append(tweet)
+            tweets_found += 1
 
-        #print("max id =", max_id)
+        yield new_tweets
 
         page_number += 1
         if page_limit and page_number > page_limit:
-            print("page limit reached (", page_number, ")")
+            print(f"Page limit reached ({page_number})")
             break
 
         if not max_id:
-            print("could not get max id, breaking")
+            print("End reached, breaking")
             break
 
         # do not include last seen tweet in next search
         max_id = str(int(max_id) - 1)
-
-
-def parse_tweet(tweet_html):
-    line = []
-    tweet_id = tweet_html.get("data-tweet-id").strip()
-    line.append(tweet_id)
-    thread_start = tweet_html.get("data-conversation-id").strip()
-    line.append(thread_start)
-    for media in tweet_html.select(".js-stream-tweet .AdaptiveMediaOuterContainer"):
-        for image in media.select("img"):
-            image_url = image.get("src").strip()
-            line.append(f"img:{image_url}")
-        for video in media.select("video"):
-            #TODO: check how youtube-dl does video extraction for twitter
-            line.append("video:yes")
-
-    text_container = tweet_html.select(".js-tweet-text")[0]
-    text_container_str = str(text_container)
-
-    # here's a little tip from me: don't use anything other than css selectors with bs
-    # I wasted about an hour trying to get selecting direct children to work
-    # and it was returning the whole container unchanged no matter what I did
-    for element in text_container.select("p > *"):
-        if element.name == "a":
-            if "twitter-atreply" in element.attrs["class"]:
-                element_text = element.text
-            elif "twitter-hashtag" in element.attrs["class"]:
-                element_text = element.text
-            elif "twitter-cashtag" in element.attrs["class"]:
-                element_text = element.text
-            elif "twitter-timeline-link" in element.attrs["class"]:
-                if "data-expanded-url" in element.attrs:
-                    element_text = element.get("data-expanded-url")
-                    if "u-hidden" in element.attrs["class"]:
-                        # add padding to avoid smashing into text
-                        element_text = f" {element_text}"
-                else:
-                    element_text = ""
-            else:
-                print(f"ID={tweet_id} LINK NOT MATCHED")
-                LOGGER.error("LINK WAS NOT MATCHED IN ID %S", tweet_id)
-                LOGGER.error("%s", element)
-        elif element.name == "span":
-            if "data-original-codepoint" in element.attrs:
-                # as far as I know, this is only done for U+fe0f
-                element_text = chr(int(element.get('data-original-codepoint')[2:], 16))
-            elif "twitter-hashflag-container" in element.attrs["class"]:
-                # this is for promotional hashtags with special "emojis" (they're not actually emojis)
-                a = element.find("a")
-                element_text = a.text if a else ""
-            else:
-                print(f"ID={tweet_id} SPAN NOT MATCHED")
-                LOGGER.error("SPAN WAS NOT MATCHED IN ID %S", tweet_id)
-                LOGGER.error("%s", element)
-        elif element.name == "img":
-            # this is for emojis - grab the alt text containing actual unicode text
-            # and disregard the image
-            element_text = element.get("alt")
-        else:
-            print(f"ID={tweet_id} TAG UNEXPECTED")
-            LOGGER.error("TAG WAS UNEXPECTED IN ID %S", tweet_id)
-            LOGGER.error("%s", element)
-
-        text_container_str = text_container_str.replace(str(element), element_text, 1)
-
-    text_container = bs4.BeautifulSoup(text_container_str, HTML_PARSER)
-    line.append(f"text:{text_container.text}")
-
-    return tweet_id, ",".join(line)
--- tweetarchiver/__main__.py
+++ tweetarchiver/__main__.py
@@ -1,17 +1,24 @@
 import sys
-import time
 import logging
+
+from sqlalchemy.orm import sessionmaker
+
 import tweetarchiver
 
-LOGGER = logging.getLogger(__name__)
-def main():
+LOGGER = logging.getLogger()
+
+def main() -> None:
+    # TODO: implement an actual cli using argparse
     args = sys.argv[1:]
     print("Received args:", args)
     if not args or len(args) > 1:
-        print("Receivedwrong number of arguments (expecting one username)")
+        print("Received wrong number of arguments (expecting one username)")
         while True:
             try:
-                args = input("Enter the name of the account you want to scrape (no spaces, no @ symbol) or press CTRL+C to cancel:").strip().split()
+                args = input(
+                    "Enter the name of the account you want to scrape"
+                    " (no spaces, no @ symbol) or press CTRL+C to cancel:"
+                    ).strip().split()
             except KeyboardInterrupt:
                 sys.exit()
             if not args:
@@ -24,14 +31,29 @@ def main():
             break
 
     username = args[0]
-    time_now_str = time.strftime("%Y-%m-%d %H:%M:%S")
-    with open(f"./twitter-{username}-scraped-{time_now_str}.csv", mode="w") as id_file:
-        tweetarchiver.scrape_tweet_ids(id_file, username)
+    dbname = f"{username}_twitter_archive.sqlite"
+    sqla_engine = tweetarchiver.sqla.create_engine(f"sqlite:///{dbname}", echo=False)
+    tweetarchiver.DeclarativeBase.metadata.create_all(sqla_engine)
+    Session = sessionmaker(bind=sqla_engine)
+    LOGGER.info("Creating new db session")
+    session = Session()
+
+    try:
+        for new_tweets in tweetarchiver.scrape_tweets(username):
+            session.add_all(new_tweets)
+            session.commit()
+    except:
+        LOGGER.info("Uncaught exception, rolling back db session")
+        session.rollback()
+        raise
+    finally:
+        LOGGER.info("Closing db session")
+        session.close()
 
 
 if __name__ == "__main__":
     try:
         main()
     except:
-        LOGGER.error()
+        LOGGER.exception("UNEXPECTED ERROR")
         raise
