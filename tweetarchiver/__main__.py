08a4a5cbee3059e95b682528ef461cb19154f681
rmmbear <rmmbear@users.noreply.github.com>
2020-01-15 01:55:50 +0100

    Shoddily implement attachment archival, change db location

--- tweetarchiver/__main__.py
+++ tweetarchiver/__main__.py
@@ -1,4 +1,5 @@
 import time
+import shutil
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -33,10 +34,13 @@ PARSER.add_argument(
 # I'm not really sure why that happens
 LOGGER = tweetarchiver.LOGGER
 
+WORKING_DIR = Path.home() / "tweetarchiver"
+WORKING_DIR.mkdir(exist_ok=True)
+
 
 def update_tweets(username: str, session: Session) -> int:
-    newest_id = tweetarchiver.TweetHTML.newest_tweet(session)
-    oldest_id = tweetarchiver.TweetHTML.oldest_tweet(session)
+    newest_id = tweetarchiver.Tweet.newest_tweet(session)
+    oldest_id = tweetarchiver.Tweet.oldest_tweet(session)
     attachment_rows = 0
     tweet_rows = 0
     start_time = time.time()
@@ -45,9 +49,11 @@ def update_tweets(username: str, session: Session) -> int:
         # only get tweets older than what's already in db
         options.append({"max_id":oldest_id})
     if oldest_id:
-        options.append({"min_id":newest_id}) # only get newer stuff
+        # only get newer stuff
+        options.append({"min_id":newest_id})
     if not options:
-        options = [{}] # get it all
+        # get it all
+        options = [{}]
 
     for kwargs in options:
         for html_page in tweetarchiver.scrape_tweets(username, **kwargs):
@@ -84,12 +90,66 @@ def update_tweets(username: str, session: Session) -> int:
     return attachment_rows + tweet_rows
 
 
-def update_media(session: Session) -> int:
-    query = session.query(tweetarchiver.Attachment).\
-        filter(tweetarchiver.Attachment.path != None).order_by(tweetarchiver.Attachment.tweet_id)
+# decide the structure:
+# one db per account, displaying full threads requires joining dbs
+# one db per main account, context tweets from other accounts stored alongside
+#
+def update_media(session: Session, archive_dir: Path) -> int:
+    attachments_path = archive_dir / "attachments"
+    attachments_path.mkdir(exist_ok=True)
+    gifs_path = attachments_path / "gif"
+    gifs_path.mkdir(exist_ok=True)
+    imgs_path = attachments_path / "img"
+    imgs_path.mkdir(exist_ok=True)
+    vids_path = attachments_path / "vid"
+    vids_path.mkdir(exist_ok=True)
+    tmpdir = archive_dir / "tmp"
+    tmpdir.mkdir(exist_ok=True)
 
-    attachments_path = Path("./media")
+    pending_attachments = tweetarchiver.Attachment.with_missing_files(session)
+    for attachment in pending_attachments:
+        if attachment.type == "vid:mp4":
+            LOGGER.warning("VIDEO DOWNLOAD CURRENTLY NOT IMPLEMENTED, SKIPPING")
+            continue
 
+        filename = attachment.url.rsplit("/", maxsplit=1)[-1]
+        temp_download = tmpdir / filename
+        if attachment.type.startswith("img"):
+            url = f"{attachment.url}:orig"
+        else:
+            url = attachment.url
+
+        with temp_download.open(mode="wb") as download_destination:
+            md5sum = tweetarchiver.download(url, to_file=download_destination)
+
+        downloaded_file_size = temp_download.stat().st_size
+
+        matching_hash_query = session.query(tweetarchiver.Attachment).filter(tweetarchiver.Attachment.hash == md5sum)
+        known_file = matching_hash_query.first()
+        if known_file:
+            LOGGER.debug("Duplicate file found")
+            LOGGER.debug("known url:%s, duplicate url:%s, hash:%s", known_file.url, attachment.url, md5sum)
+            assert known_file.size == downloaded_file_size
+            attachment.size = known_file.size
+            attachment.hash = md5sum
+            attachment.path = known_file.path
+            temp_download.unlink()
+        else:
+            if attachment.type.startswith("img"):
+                final_file_path = imgs_path / filename
+            elif attachment.type == "vid:gif":
+                final_file_path = gifs_path / filename
+            else:
+                final_file_path = vids_path / filename
+
+            shutil.move(temp_download, final_file_path)
+            attachment.size = downloaded_file_size
+            attachment.hash = md5sum
+            attachment.path = str(final_file_path.relative_to(archive_dir))
+
+        session.commit()
+
+    return
 
 
 def scraper_test() -> bool:
@@ -111,19 +171,23 @@ def main() -> None:
         assert scraper_test()
 
     username = args.username.lower()
-    dbname = f"{username}_twitter_archive.sqlite"
-    dbexists = Path(dbname).exists()
-    sqla_engine = tweetarchiver.sqla.create_engine(f"sqlite:///{dbname}", echo=False)
+    dbpath = WORKING_DIR / username
+    dbpath.mkdir(exist_ok=True)
+    dbfile = dbpath / f"{username}_twitter_archive.sqlite"
+    dbexists = Path(dbpath).exists()
+
+    sqla_engine = tweetarchiver.sqla.create_engine(f"sqlite:///{str(dbfile)}", echo=False)
     tweetarchiver.DeclarativeBase.metadata.create_all(sqla_engine)
     bound_session = sessionmaker(bind=sqla_engine)
     LOGGER.info("Creating new db session")
     session = bound_session()
 
     try:
-        if not args.skip_tweets and not args.skip_update:
-            update_tweets(username, session)
-        if (not args.skip_images or not args.skip_videos) and not args.skip_update:
-            update_media(session)
+        if not args.skip_update:
+            if not args.skip_tweets:
+                update_tweets(username, session)
+            if not args.skip_images or not args.skip_videos:
+                update_media(session, dbpath)
         if args.export:
             export(session)
     except:
@@ -150,4 +214,3 @@ if __name__ == "__main__":
     # TODO: account for possible changes of handle/displayname
     # TODO: save conversation context
     # TODO: save archive to ~/tweetarchiver
-
